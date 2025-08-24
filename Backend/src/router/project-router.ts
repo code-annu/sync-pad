176a299ab0318bbd75e1b86a33225d25ddfda972
Anurag Rajput <code.annu@gmail.com>
2025-08-24 16:11:24 +0530

    Extract path param from router file

--- Backend/src/router/project-router.ts
+++ Backend/src/router/project-router.ts
@@ -4,12 +4,9 @@ import { Router } from "express";
 export const projectRouter = Router();
 const projectController = new ProjectController();
 
-projectRouter.post(
-  "/projects",
-  projectController.projectsPost.bind(projectController)
-);
+projectRouter.post("/", projectController.projectsPost.bind(projectController));
 
 projectRouter.get(
-  "/projects/:projectId",
+  "/:projectId",
   projectController.projectsGet.bind(projectController)
 );
